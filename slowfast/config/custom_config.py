8ea866b7b6d40e210e82cc7532cb268cbe4acdf3
Andrés Ramírez Quirós <andres.ramirez+github@ridgerun.com>
2021-05-14 09:36:54 -0600

    Add custom params for transfer learning in cfg
    New parameters include:
    - Method to use for transfer learning
    - Learning rate multiplier
    - Force old head to avoid reseting head for the execution

--- slowfast/config/custom_config.py
+++ slowfast/config/custom_config.py
@@ -2,8 +2,11 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 """Add custom configs and default values"""
-
+from fvcore.common.config import CfgNode
 
 def add_custom_config(_C):
     # Add your own customized configs.
-    pass
+    _C.TL = CfgNode()
+    _C.TL.METHOD = "ft"
+    _C.TL.LR_MULTIPLIER = 1e-1
+    _C.TL.FORCE_OLD_HEAD = False
